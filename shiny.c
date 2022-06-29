0225074aedecda06a3ba8034fa3120fe98108feb
Hitarth Thummar <hitarth.thummar@protonmail.com>
2022-06-29 21:39:47 +0530

    memory leak fixed

--- shiny.c
+++ shiny.c
@@ -1,17 +1,17 @@
 /*	
 	SHINY - An HTML to GemText converter
 	====================================
-WIP: Please provide suggestions to:
-gtlsgamr<gtlsgamr@tilde.team> or 
-gtlsgamr<gtlsgamr@ctrl-c.club>  
-====================================
-At this point, the program assumes a few things:
- * You will not nest tags on the same line.
- * You will start anchor tag attributes with the href attribute:
- <a href=""..... > </a>   <---- Correct
- <a class="" href=""..... > </a>   <---- Correct
- * One type of tag on one line, if possible.
- * You will enter html without meta data. It might work with it, but try to input only the stuff that you want to convert and nothing else.
+	WIP: Please provide suggestions to:
+	gtlsgamr<gtlsgamr@tilde.team> or 
+	gtlsgamr<gtlsgamr@ctrl-c.club>  
+	====================================
+	At this point, the program assumes a few things:
+		 * You will not nest tags on the same line.
+		 * You will start anchor tag attributes with the href attribute:
+		 <a href=""..... > </a>   <---- Correct
+		 <a class="" href=""..... > </a>   <---- Correct
+		 * One type of tag on one line, if possible.
+		 * You will enter html without meta data. It might work with it, but try to input only the stuff that you want to convert and nothing else.
  */
 
 #include <stdio.h>
@@ -163,4 +163,5 @@ void parser() {
 			printf("%c",c);
 		}
 	}
+	free(curr_tag);
 }
