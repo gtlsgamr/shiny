a7efa61ac565a9b0b29ec021373f133f5eb77727
Hitarth Thummar <hitarth.thummar@protonmail.com>
2022-06-29 19:44:51 +0530

    Made the code a bit better

--- shiny.c
+++ shiny.c
@@ -1,10 +1,10 @@
 /*	
 	SHINY - An HTML to GemText converter
-	------------------------------------
+	====================================
 WIP: Please provide suggestions to:
 gtlsgamr<gtlsgamr@tilde.team> or 
 gtlsgamr<gtlsgamr@ctrl-c.club>  
-------------------------------------
+====================================
 At this point, the program assumes a few things:
  * You will not nest tags on the same line.
  * You will start anchor tag attributes with the href attribute:
@@ -15,136 +15,114 @@ At this point, the program assumes a few things:
  */
 
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <ctype.h>
 
-int in_tag = 0;
-int btn_tag = 0;
-void tagparser(char c) {
-	switch (c) {
-		case 'a':
-			printf("\n");
-			switch (getchar()) {
-				case ' ':
-					if (getchar() == 'h') {
-						printf("=> ");
-						for (c = getchar(); c != '='; c = getchar()) {}
-						getchar();
-						for (c = getchar(); c != 39 && c != '"'; c = getchar()) {
-							printf("%c", c);
-						}
-						printf(" ");
-						getchar();
-						for (c = getchar(); c != '<'; c = getchar()) {
-							printf("%c", c);
-						}
-						printf("\n");
-						in_tag = 1;
-					} else {
-						for (c = getchar(); c != ' ' && getchar() != 'h'; c = getchar()) {}
-						printf("=> ");
-						for (c = getchar(); c != '='; c = getchar()) {}
-						getchar();
-						for (c = getchar(); c != 39 && c != '"'; c = getchar()) {
-							printf("%c", c);
-						}
-						printf(" ");
-						getchar();
-						for (c = getchar(); c != '<'; c = getchar()) {
-							printf("%c", c);
-						}
-						printf("\n");
-						in_tag = 1;
-					}
-					break;
-				default:
-					;
-			}
-			break;
+int in_tag=0;
 
-		case 'l':
-			switch (getchar()) {
-				case 'i':
-					printf("* ");
-					for (c = getchar(); c != '>'; c = getchar()) {}
-					for (c = getchar(); c != '<'; c = getchar()) {
-						printf("%c", c);
-					}
-					btn_tag = 0;
-					in_tag = 1;
-					break;
-				default:
-					break;
-			}
-			break;
-
-		case 'h':
-			in_tag = 1;
-			btn_tag = 0;
-			switch (getchar()) {
-				case '1':
-					printf("%c ", '#');
-					break;
-				case '2':
-					printf("%c%c ", '#', '#');
-					break;
-				case '3':
-					printf("%c%c%c ", '#', '#', '#');
-					break;
-				case '4':
-					printf("%c%c%c ", '#', '#', '#');
-					break;
-				case '5':
-					printf("%c%c%c ", '#', '#', '#');
-					break;
-				case '6':
-					printf("%c%c%c ", '#', '#', '#');
-					break;
-				default:
-					break;
-			}
-			break;
-		case 'b':
-			for (c = getchar(); c != '>'; c = getchar()) {}
-			break;
-		case 'p':
-			break;
-		default:
-			;
-	}
-}
-
-void parser() {
-	int c;
-	while ((c = getchar()) != EOF) {
-		switch (c) {
-			case '<':
-				in_tag = 1;
-				c = getchar();
-				tagparser(c);
-				break;
-
-			case '>':
-				btn_tag = in_tag ? 1 : 0;
-				in_tag = 0;
-				c = getchar();
-				break;
-
-			default:
-				;
-		}
-		if (in_tag == 0 && btn_tag == 1) {
-			printf("%c", c);
-		}
-	}
-
-}
+void gettag(char *S);
+void tagparser(char *tag);
+void parser();
 
 int main(int argc, char * argv[]) {
 	parser();
 	return 0;
+}
+
+void gettag(char *S){
+	int c,i=0;
+	while((c=getchar())!=' ' && c!='>'){
+		if(c=='/')return;
+		*(S+i)=c;
+		i++;
+	}
+	*(S+i)='\0';
+}
+
+void tagparser(char *tag) {
+	in_tag=1;
+	int c=getchar();
+
+	if(strcmp(tag,"a")==0){
+		printf("\n=> ");
+		for(;(c!=39&&c!='"');c=getchar());
+		c=getchar();
+		for(;c!=39 && c!='"';c=getchar()){
+			printf("%c",c);
+		}
+		for(;c!='>';c=getchar());
+		printf(" ");
+		for(c=getchar();c!='<';c=getchar()){
+			printf("%c",c);
+		}
+		for(;(c!='>');c=getchar());
+		printf("\n");
+	}
+
+	if(strncmp(tag,"h",1)==0){
+		switch(tag[1]){
+			case '1': printf("# "); 
+					  break;
+			case '2': printf("## "); 
+					  break;
+			case '3': printf("### "); 
+					  break;
+			case '4': printf("### "); 
+					  break;
+			case '5': printf("### "); 
+					  break;
+			case '6': printf("### "); 
+					  break;
+			default : break;
+		}
+		for(;c!='<';c=getchar()){
+			printf("%c",c);
+		}
+		for(;(c!='>');c=getchar());
+		printf("\n");
+
+	}
+
+	if(strcmp(tag,"p")==0){
+		printf("%c",c);
+		for(c=getchar();c!='<';c=getchar()){
+			printf("%c",c);
+		}
+		for(;(c!='>');c=getchar());
+		printf("\n");
+	}
+
+	in_tag=0;
+}
+
+void parser() {
+	int c,d;
+	char *curr_tag;
+	curr_tag = (char *)malloc(sizeof(char)*8);
+	while ((c = getchar()) != EOF) {
+		if(c=='\n'){
+			printf("%c",c);
+			d = getchar();
+			while(isspace(d)){
+				d=getchar();
+			}
+			if(d=='<' && in_tag==0){
+				gettag(curr_tag);
+				tagparser(curr_tag);
+			}
+			else{
+				printf("%c",d);
+			}
+
+			parser();
+		}
+		else if(c=='<' && in_tag==0){
+			gettag(curr_tag);
+			tagparser(curr_tag);
+		}
+		else{
+			printf("%c",c);
+		}
+	}
 }
